2cc131fd94acbc174cef46341559a80b7b72a6c2
Pankaj20202024 <pankajsinghr1407@gmail.com>
2023-08-02 19:25:32 +0530

    commit change

---
+++ src/component/list/List.jsx
@@ -0,0 +1,90 @@
+import ListRow from "./ListRow";
+import ListRowCell from "./ListRowCell";
+
+import ListHeader from "./ListHeader";
+import ListHeaderCell from "./ListHeaderCell";
+
+import styles from "./List.module.css";
+import { useState } from "react";
+
+
+const List = ({ rows, SelectedCurrency, searchText, onRowSelect }) => {
+  const filteredRows = searchText
+    ? rows.filter((row) =>
+        row["&id"].toLowerCase().includes(searchText.toLowerCase())
+      )
+    : rows;
+
+  const [selectedRowId, setSelectedRowId] = useState(null);
+
+  const handleRowClick = (rowId) => {
+    setSelectedRowId(rowId);
+    const selectedRow = rows.find((row) => row["&id"] === rowId);
+    onRowSelect(selectedRow);
+  };
+
+  return (
+    <>
+      <table className={styles.container}>
+        <thead>
+          <ListHeader>
+            <ListHeaderCell>Order ID</ListHeaderCell>
+            <ListHeaderCell>Buy/Sell</ListHeaderCell>
+            <ListHeaderCell>Country</ListHeaderCell>
+            <ListHeaderCell>Order Submitted</ListHeaderCell>
+            <ListHeaderCell>Order Volume / {SelectedCurrency}</ListHeaderCell>
+          </ListHeader>
+        </thead>
+        <tbody>
+          {filteredRows.map((row) => (
+            <ListRow
+              key={row["&id"]}
+              onClick={() => handleRowClick(row["&id"])}
+              isSelected={selectedRowId === row["&id"]}
+            >
+              <ListRowCell>{row["&id"]}</ListRowCell>
+              <ListRowCell>{row.executionDetails.buySellIndicator}</ListRowCell>
+              <ListRowCell>{row.executionDetails.orderStatus}</ListRowCell>
+              <ListRowCell>
+                {row.timestamps?.orderSubmitted || "N/A"}
+              </ListRowCell>
+              {/* FOR USD */}
+              {SelectedCurrency === "USD" && (
+                <ListRowCell>
+                  {row.bestExecutionData.orderVolume.USD}
+                </ListRowCell>
+              )}
+
+              {/* FOR GBP */}
+              {SelectedCurrency === "GBP" && (
+                <ListRowCell>
+                  {row.bestExecutionData.orderVolume.GBP}
+                </ListRowCell>
+              )}
+
+              {/* FOR JPY */}
+              {SelectedCurrency === "JPY" && (
+                <ListRowCell>
+                  {row.bestExecutionData.orderVolume.JPY}
+                </ListRowCell>
+              )}
+
+              {/* FOR EUR */}
+              {SelectedCurrency === "EUR" && (
+                <ListRowCell>
+                  {row.bestExecutionData.orderVolume.EUR}
+                </ListRowCell>
+              )}
+
+              {!["USD", "JPY", "EUR", "GBP"].includes(SelectedCurrency) && (
+                <ListRowCell>N/A</ListRowCell>
+              )}
+            </ListRow>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default List;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+
+// Data
+import mockData from "../assets/data.json";
+import timestamps from "../assets/timeStamps.json";
+
+// Components
+import Dropdown from "../component/dropdown/Dropdown";
+import HeaderTitle from "../component/header-title/HeaderTitle";
+import Search from "../component/search/Search";
+import List from "../component/list/List";
+
+// Styles
+import styles from "./Dashboard.module.css";
+import Card from "../component/card/Card";
+
+const Dashboard = () => {
+  const [currency, setCurrency] = useState("USD");
+  const [searchText, setSearchText] = useState("");
+  const [selectedOrderDetails, setSelectedOrderDetails] = useState({});
+  const [selectedOrderTimeStamps, setSelectedOrderTimeStamps] = useState({});
+
+  const mergeTimestampsWithOrders = () => {
+    const mergedData = mockData.results.map((order, index) => {
+      const timestampsData = timestamps.results[index]?.timestamps;
+      if (timestampsData) {
+        return { ...order, timestamps: timestampsData };
+      }
+      return order;
+    });
+    return { ...mockData, results: mergedData };
+  };
+
+  const dataWithTimestamps = mergeTimestampsWithOrders();
+
+  // Function to handle order selection
+  const handleRowSelect = (order) => {
+    setSelectedOrderDetails({
+      buySellIndicator: order.executionDetails.buySellIndicator,
+      orderStatus: order.executionDetails.orderStatus,
+      orderType: order.executionDetails.orderType,
+    });
+
+    setSelectedOrderTimeStamps({
+      orderRecieved: order.timestamps.orderReceived,
+      orderStatusUpadate: order.timestamps.orderStatusUpdated,
+      orderSubmitted: order.timestamps.orderSubmitted,
+    });
+  };
+
+  return (
+    <div>
+      <div className={styles.header}>
+        <HeaderTitle
+          primaryTitle="Orders"
+          secondaryTitle={mockData.header.returnedHits + " orders"}
+        />
+        <div className={styles.actionBox}>
+          <Search
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <Dropdown
+            options={["GBP", "USD", "JPY", "EUR"]}
+            onChange={(e) => setCurrency(e.target.value)}
+            selectedItem={currency}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.section}>
+          <Card
+            cardData={selectedOrderDetails}
+            title="Selected Order Details"
+          />
+          <Card
+            cardData={selectedOrderTimeStamps}
+            title="Selected Order Timestamps"
+          />
+        </div>
+
+        <List
+          rows={dataWithTimestamps.results}
+          SelectedCurrency={currency}
+          searchText={searchText}
+          onRowSelect={handleRowSelect}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
